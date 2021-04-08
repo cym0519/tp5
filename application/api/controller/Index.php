4f458e72a80e83016a2a7c8db16d001bdcdb9837
cym0519 <986306442@qq.com>
2021-04-08 11:13:19 +0800

    对接微信测试

--- application/api/controller/Index.php
+++ application/api/controller/Index.php
@@ -2,6 +2,7 @@
 
 
 namespace app\api\controller;
+use think\console\input\Option;
 use think\Controller;
 use think\Request;
 use think\Session;
@@ -56,5 +57,22 @@ class Index extends Controller
         $data=today();
         $this->assign('data',$data);
         return view();
+    }
+    public function dockweixin(){
+        //将timestamp,nonce，token按字典排序
+        $timestamp = $_GET['timestamp'];
+        $nonce     = $_GET['nonce'];
+        $token     = 'weixin';
+        $signature = $_GET['signature'];
+        $array     = array($timestamp, $nonce, $token);
+        sort($array);
+        //将排序后的三个参数拼接之后用sha1加密
+        $tmpstr = implode('',$array);
+        $tmpstr = sha1($tmpstr);
+        //将加密后的字符串与signature进行对比，判断该请求是否来自微信
+        if($tmpstr == $signature){
+            echo $_GET['echostr'];
+            exit;
+        }
     }
 }
