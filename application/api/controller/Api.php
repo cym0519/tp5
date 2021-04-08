4f3c32b6bcd68ed92454999a43c99e9a62e955d8
cym0519 <986306442@qq.com>
2021-04-08 11:31:44 +0800

    修改文件

--- application/api/controller/Api.php
+++ application/api/controller/Api.php
@@ -184,7 +184,22 @@ class Api extends Controller{
         }else {
             dump($validate->getError());
         }
-        
     }
-
+    public function dockweixin(){
+        //将timestamp,nonce，token按字典排序
+        $timestamp = $_GET['timestamp'];
+        $nonce     = $_GET['nonce'];
+        $token     = 'weixin';
+        $signature = $_GET['signature'];
+        $array     = array($timestamp, $nonce, $token);
+        sort($array);
+        //将排序后的三个参数拼接之后用sha1加密
+        $tmpstr = implode('',$array);
+        $tmpstr = sha1($tmpstr);
+        //将加密后的字符串与signature进行对比，判断该请求是否来自微信
+        if($tmpstr == $signature){
+            echo $_GET['echostr'];
+            exit;
+        }
+    }
 }
